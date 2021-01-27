b2f380fa253ca7cf82d4b54f70816ecc7d7d4000
Caleb Trachte <cetrachte@hotmail.com>
2021-01-27 13:12:51 -0600

    add basic components and styling

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press me!" @press="exclaim" />
+    <button class="alert" v-bind:title="message" v-bind:on-press="handleBtnPress"/>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    },
+    handleBtnPress: function() {
+      alert('Btn Press');
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+button {
+  margin:10%;
+}
+</style>
